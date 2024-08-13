d1baa1eb1ed2b811e47c7e34d6c33e3f659a7ff0
manhkl1812 <manhkl1025@gmail.com>
2024-08-13 17:05:43 +0700

    Welcome class

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello!\n");
+        String hoten = "Dinh Duc Manh";
+        String class_name = "DHKTPM19ATT";
+        System.out.printf("Ho va ten: %s\nLop hoc phan: %s", hoten, class_name);
+
+    }
+}
