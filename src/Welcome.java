fd50d79c3c9c366cb969d6d87a34883325525786
manhkl1812 <manhkl1025@gmail.com>
2024-08-13 17:41:01 +0700

    LAB1

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class Welcome {
+    public static void main(String[] args) {
+        System.out.printf("Hello!\n");
+        String hoten = "Dinh Duc Manh";
+        String class_name = "DHKTPM19ATT";
+        System.out.printf("Ho va ten: %s\nLop hoc phan: %s", hoten, class_name);
+
+    }
 }
